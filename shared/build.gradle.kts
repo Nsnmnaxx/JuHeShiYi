9e364fcfc3eece753e8606f501f915e5c2e5d639
Nnnmn <cnm1314202106@163.com>
2026-07-29 01:12:08 +0800

    fix: add kotlin.compose plugin to shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.kotlin.serialization)
 }
