229b8eeee0d85d191d83b924fa1ecb0e66732352
Nnnmn <cnm1314202106@163.com>
2026-07-29 01:04:38 +0800

    fix: remove duplicate compose compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.compose.multiplatform) apply false
-    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.spotless)
 }
 
