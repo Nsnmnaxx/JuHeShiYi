c58f5d9d14256036323b6e0b7a6556e9e8ac89f7
Nnnmn <cnm1314202106@163.com>
2026-07-30 02:27:24 +0800

    fix: use Composable get for font families in Type.kt

--- shared/src/commonMain/kotlin/com/lyihub/archiveassistant/ui/theme/Type.kt
+++ shared/src/commonMain/kotlin/com/lyihub/archiveassistant/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.lyihub.archiveassistant.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -9,9 +10,11 @@ import androidx.compose.ui.unit.sp
 import com.lyihub.archiveassistant.shared.generated.resources.*
 import org.jetbrains.compose.resources.Font
 
-val ImperialTitleFont = FontFamily(Font(Res.font.san_ji_xing_kai_jian_ti_cu, FontWeight.Normal))
+val ImperialTitleFont: FontFamily
+    @Composable get() = FontFamily(Font(Res.font.san_ji_xing_kai_jian_ti_cu, FontWeight.Normal))
 
-val ImperialDisplayFont = FontFamily(Font(Res.font.dinglie_song_typeface, FontWeight.Normal))
+val ImperialDisplayFont: FontFamily
+    @Composable get() = FontFamily(Font(Res.font.dinglie_song_typeface, FontWeight.Normal))
 
 val ImperialStampTitleFont = ImperialTitleFont
 
