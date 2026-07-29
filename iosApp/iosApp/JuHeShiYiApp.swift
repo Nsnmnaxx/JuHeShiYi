025a3e13a15191c87ae4b1f1f5f662a43e604a2e
Nnnmn <cnm1314202106@163.com>
2026-07-30 03:05:53 +0800

    fix: remove nonexistent SharedModuleInitializerKt call

--- iosApp/iosApp/JuHeShiYiApp.swift
+++ iosApp/iosApp/JuHeShiYiApp.swift
@@ -3,11 +3,6 @@ import Shared
 
 @main
 struct JuHeShiYiApp: App {
-    init() {
-        // 初始化 KMP shared 模块
-        SharedModuleInitializerKt.doInitKoin()
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
