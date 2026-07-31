a6bf8946948a2b29ea6eabd3facd8fde0d9780a2
Nnnmn <cnm1314202106@163.com>
2026-07-31 14:06:44 +0800

    fix: remove coreLibraryDesugaringEnabled from shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -74,7 +74,6 @@ android {
     }
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
